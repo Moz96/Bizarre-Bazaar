11b1f2ed310e1674165192a515c483d057e48845
charlie morris <charliemorris.96@gmail.com>
2023-05-15 14:48:46 +0100

    Updated booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,40 @@
 class BookingsController < ApplicationController
-  def index
-    @bookings = Booking.all
-  end
+  before_action :set_item, only: [:new, :create]
+  before_action :set_booking, only: [:show, :destroy]
 
   def new
-    @booking = Booking.new
+    @booking = @item.bookings.new
   end
 
   def create
-    @booking = Booking.new(booking_params)
+    @booking = @item.bookings.new(booking_params)
     if @booking.save
-      redirect_to root_path
+      redirect_to item_booking_path(@item, @booking), notice: 'Booking was successfully created.'
     else
-      flash.now[:alert] = "Booking could not be created"
+      flash.now[:alert] = 'Booking could not be created.'
       render :new
     end
   end
 
+  def show
+  end
+
   def destroy
-    @booking = Booking.find(params[:id])
     @booking.destroy
-    redirect_to root_path
+    redirect_to root_path, notice: 'Booking was successfully deleted.'
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :item_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :items, only: [:new, :create, :show, :destroy ]
-  resources :bookings, only: [:new, :create, :show, :destroy]
+  resources :items, only: [:new, :create, :show, :destroy ] do
+    resources :bookings, only: [:new, :create, :show, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

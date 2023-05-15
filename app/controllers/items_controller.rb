fc05719bdc215462d0d73b6b099f72b573ac8d9d
charlie morris <charliemorris.96@gmail.com>
2023-05-15 13:44:48 +0100

    items controller added

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,35 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'Item could not be created'
+      render :new
+    end
+  end
+
+  def show
+    @items = Item.find(params[:id])
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :description)
+  end
+end

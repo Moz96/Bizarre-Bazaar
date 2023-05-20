c6217e65855fdc3c37e767c9787372e2eb382ff8
Nick Hennell-Foley <nicholas@nicholashennellfoley.com>
2023-05-20 13:08:37 +0100

    Required openai in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "openai"
+
 puts "Cleaning database"
 
 Booking.destroy_all

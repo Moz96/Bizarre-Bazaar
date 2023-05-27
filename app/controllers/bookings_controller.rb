589bbcd343b85788cd0a50c43769155eb077b04d
charlie morris <charliemorris.96@gmail.com>
2023-05-27 12:23:32 +0100

    removed changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,7 +9,6 @@ class BookingsController < ApplicationController
 
   def new
     @booking = @item.bookings.new
-    @booking.start_date = params[:start_date] if params[:start_date].present?
   end
 
   def create
